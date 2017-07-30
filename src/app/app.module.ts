76f01086bca8dda5399c71dec6788a259093263f
Fabio Herval <fabioherval@gmail.com>
2017-07-30 19:14:40 -0300

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,83 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+
+import { AngularFireModule, FirebaseAppConfig, AuthProviders, AuthMethods } from 'angularfire2';
+
+import { MyApp } from './app.component';
+import { CustomLoggedHeaderComponent } from './../components/custom-logged-header/custom-logged-header.component';
+import { MessageBoxComponent } from './../components/message-box/message-box.component';
+import { ProgressBarComponent } from './../components/progress-bar/progress-bar.component';
+import { UserInfoComponent } from './../components/user-info/user-info.component';
+import { UserMenuComponent } from './../components/user-menu/user-menu.component';
+
+import { AuthService } from './../providers/auth.service';
+import { ChatService } from './../providers/chat.service';
+import { MessageService } from './../providers/message.service';
+import { UserService } from './../providers/user.service';
+
+import { CapitalizePipe } from './../pipes/capitalize.pipe';
+
+import { HomePage } from '../pages/home/home';
+import { SigninPage } from './../pages/signin/signin';
+import { SignupPage } from './../pages/signup/signup';
+import { ChatPage } from './../pages/chat/chat';
+import { UserProfilePage } from './../pages/user-profile/user-profile';
+
+const firebaseAppConfig: FirebaseAppConfig = {
+  apiKey: "AIzaSyBGKIWvO8YySw4sWlSgv7v5xHY1n9h5qSQ",
+  authDomain: "ionic2-firebase-chat-b0c65.firebaseapp.com",
+  databaseURL: "https://ionic2-firebase-chat-b0c65.firebaseio.com",
+  storageBucket: "ionic2-firebase-chat-b0c65.appspot.com",
+  messagingSenderId: "968689869506"
+};
+
+const firebaseAuthConfig = {
+  provider: AuthProviders.Custom,
+  method: AuthMethods.Password
+}
+
+@NgModule({
+  declarations: [
+    CapitalizePipe,
+    CustomLoggedHeaderComponent,
+    MyApp,
+    HomePage,
+    MessageBoxComponent,
+    ProgressBarComponent,
+    SigninPage,
+    SignupPage,
+    UserInfoComponent,
+    UserMenuComponent,
+    ChatPage,
+    UserProfilePage
+  ],
+  imports: [
+    BrowserModule,
+    HttpModule,
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseAppConfig, firebaseAuthConfig)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage,
+    SigninPage,
+    SignupPage,
+    ChatPage,
+    UserProfilePage
+  ],
+  providers: [
+    AuthService,
+    ChatService,
+    MessageService,
+    StatusBar,
+    SplashScreen,
+    UserService,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
---
+++ src/pages/signup/signup.ts
@@ -0,0 +1,99 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import { NavController, NavParams, Loading, LoadingController, AlertController } from 'ionic-angular';
+
+import 'rxjs/add/operator/first';
+
+import { FirebaseAuthState } from 'angularfire2';
+
+import { HomePage } from './../home/home';
+
+import { UserService } from './../../providers/user.service';
+import { AuthService } from './../../providers/auth.service';
+
+@Component({
+  selector: 'page-signup',
+  templateUrl: 'signup.html',
+})
+export class SignupPage {
+
+  signupForm: FormGroup;
+
+  constructor(
+    public alertCtrl: AlertController,
+    public authService: AuthService,
+    public formBuilder: FormBuilder,
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public loadingCtrl: LoadingController,
+    public userService: UserService
+  ) {
+
+    let emailRegex = /^[a-z0-9!#$%&'*+\/=?^_`{|}~.-]+@[a-z0-9]([a-z0-9-]*[a-z0-9])?(\.[a-z0-9]([a-z0-9-]*[a-z0-9])?)*$/i;
+
+    this.signupForm = this.formBuilder.group({
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      username: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', Validators.compose([Validators.required, Validators.pattern(emailRegex)])],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  onSubmit(): void {
+    let loading: Loading = this.showLoading();
+    let formUser = this.signupForm.value;
+    let username: string = formUser.username;
+
+    this.userService.userExists(username)
+      .first()
+      .subscribe((userExists: boolean) => {
+        if (!userExists) {
+          this.authService.createAuthUser({
+            email: formUser.email,
+            password: formUser.password
+          }).then((authState: FirebaseAuthState) => {
+
+            delete formUser.password;
+            let uuid: string = authState.auth.uid;
+
+            this.userService.create(formUser, uuid)
+              .then(() => {
+                console.log('Usuário cadastrado!');
+                this.navCtrl.setRoot(HomePage);
+                loading.dismiss();
+              }).catch((error: any) => {
+                console.log(error);
+                loading.dismiss();
+                this.showAlert(error);
+              });
+
+          }).catch((error: any) => {
+            console.log(error);
+            loading.dismiss();
+            this.showAlert(error);
+          });
+        } else {
+          this.showAlert(`O Usuário "${username}" já está sendo usado em outra conta!`);
+          loading.dismiss();
+        }
+      });
+  }
+
+  private showLoading(): Loading {
+    let loading: Loading = this.loadingCtrl.create({
+      content: 'Por favor, aguarde...'
+    });
+
+    loading.present();
+
+    return loading;
+  }
+
+  private showAlert(message: string): void {
+    this.alertCtrl.create({
+      message: message,
+      buttons: ['Ok']
+    }).present();
+  }
+
+}
